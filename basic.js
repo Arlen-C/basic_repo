eb518063d62eaf3425f6b9ff494e370dd1033ce4
Arlen-C <lencsde@gmail.com>
2021-02-09 16:53:03 -0800

    basicR

--- basic.js
+++ basic.js
@@ -3,5 +3,6 @@
 <h2>What Can JavaScript Do?</h2>
 <p id="demo">JavaScript can change HTML content.</p>
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
+<p>Added some dialogue for assingment purposes, LOL</p>
 </body>
 </html>
